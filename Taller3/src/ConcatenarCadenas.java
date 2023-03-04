39a4ff084af3f11196f91e77daaa6321279fab85
CristianRzf <cristian.ruiz03@usc.edu.co>
2023-03-03 20:46:58 -0500

    Taller3

---
+++ Taller3/src/ConcatenarCadenas.java
@@ -0,0 +1,14 @@
+
+public class ConcatenarCadenas {
+	public static void main(String[] args) {
+        // Declaración e instanciación de dos objetos de cadena independientes
+        String str1 = "A";
+        String str2 = "B";
+        
+        // Concatenación y asignación de un tercer objeto de cadena arbitrario
+        String str3 = str1 + " " + str2 ;
+        
+        System.out.println(str3);
+    }
+
+}
